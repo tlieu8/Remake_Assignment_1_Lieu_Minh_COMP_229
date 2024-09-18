9a6a0194ac0b8d54b07a1fa49d56f2b81d03bf73
tlieu8 <tlieu8@my.centennialcollege.ca>
2024-09-18 02:42:31 +0000

    Update education and contact p1

--- client/src/contact.jsx
+++ client/src/contact.jsx
@@ -1,45 +1,77 @@
 import "../src/contact.css";
 
+import React from "react";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Box,
+} from "@mui/material";
+
 export default function Contact() {
   return (
-    <>
-      <p>Contact</p>
-      <form id="ffp">
-        <label htmlFor="myFName"> *First Name: </label>
-        <input
-          type="text"
-          id="myFName"
-          name="myFName"
-          required="required"
-          autoFocus
-        />{" "}
-        <br /> <br />
-        <label htmlFor="myLName"> *Last Name: </label>
-        <input
-          type="text"
-          id="myLName"
-          name="myLName"
-          required="required"
-        />{" "}
-        <br /> <br />
-        <label htmlFor="myEmail"> *Email: </label>
-        <input
-          type="email"
-          id="myEmail"
-          name="myEmail"
-          required="required"
-        />{" "}
-        <br /> <br />
-        <input type="submit" className="submit" value="Submit" /> <br />
-      </form>{" "}
-      <br></br>
-      <p>
-        <i>
-          Call me on: <strong>888.555.5555 </strong>
-        </i>
-      </p>
-      <p>facebook:www.xyz.facebook.com</p>
-      <p>email:xyz@yahoo.com</p>
-    </>
+    <Container maxWidth="sm" sx={{ paddingY: 4 }}>
+      {/* Page Title */}
+      <Typography variant="h4" component="h1" align="center" gutterBottom>
+        Contact Me
+      </Typography>
+
+      {/* Contact Form */}
+      <form noValidate autoComplete="off">
+        <Box sx={{ marginBottom: 2 }}>
+          <TextField
+            fullWidth
+            required
+            id="myFName"
+            label="First Name"
+            variant="outlined"
+            autoFocus
+          />
+        </Box>
+        <Box sx={{ marginBottom: 2 }}>
+          <TextField
+            fullWidth
+            required
+            id="myLName"
+            label="Last Name"
+            variant="outlined"
+          />
+        </Box>
+        <Box sx={{ marginBottom: 2 }}>
+          <TextField
+            fullWidth
+            required
+            id="myEmail"
+            label="Email"
+            type="email"
+            variant="outlined"
+          />
+        </Box>
+
+        {/* Submit Button */}
+        <Box sx={{ textAlign: "center", marginTop: 3 }}>
+          <Button variant="contained" color="primary" type="submit" size="large">
+            Submit
+          </Button>
+        </Box>
+      </form>
+
+      {/* Additional Contact Info */}
+      <Box mt={4} textAlign="center">
+        <Typography variant="body1">
+          <i>
+            Call me on: <strong>888.555.5555</strong>
+          </i>
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          facebook: www.xyz.facebook.com
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          email: xyz@yahoo.com
+        </Typography>
+      </Box>
+    </Container>
   );
 }
+
--- client/src/education.jsx
+++ client/src/education.jsx
@@ -1,7 +1,50 @@
+import React from "react";
+import { Container, Typography, Card, CardContent, Grid } from "@mui/material";
+
 export default function Education() {
+  const educationData = [
+    {
+      school: "University of Example",
+      degree: "Bachelor of Science in Computer Science",
+      year: "2015 - 2019",
+      description: "Graduated with honors, specializing in software development.",
+    },
+    {
+      school: "Example High School",
+      degree: "High School Diploma",
+      year: "2011 - 2015",
+      description: "Focused on mathematics, physics, and computer science.",
+    },
+  ];
+
   return (
-    <>
-      <p>Education.Qualification</p>
-    </>
+    <Container maxWidth="lg" sx={{ paddingY: 4 }}>
+      {/* Title */}
+      <Typography variant="h4" component="h1" gutterBottom align="center">
+        Education Qualifications
+      </Typography>
+
+      {/* Education Cards */}
+      <Grid container spacing={4}>
+        {educationData.map((edu, index) => (
+          <Grid item xs={12} sm={6} key={index}>
+            <Card variant="outlined" sx={{ boxShadow: 3 }}>
+              <CardContent>
+                <Typography variant="h6" component="div">
+                  {edu.school}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  {edu.degree}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {edu.year}
+                </Typography>
+                <Typography variant="body1">{edu.description}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
